4468fca4f72cc302fa5001b9aacb480c9f22edca
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2026-04-13 20:42:40 +0300

    the update of the century

--- КН-25-1-1/смооп/LabApp3/Tasks.cs
+++ КН-25-1-1/смооп/LabApp3/Tasks.cs
@@ -9,7 +9,7 @@ namespace LabApp3
         static Random random = new Random();
         public static void Task1()
         {
-            int[] array = new int[20];
+            int[]? array = null;
 
             Console.WriteLine("1-Клавіатура, 2-файл, 3-Рандом");
             int choice = int.Parse(Console.ReadLine());
@@ -17,24 +17,31 @@ namespace LabApp3
             switch (choice)
             {
                 case 1:
-                    Console.Write("Розмір N: ");
-                    array = new int[int.Parse(Console.ReadLine())];
-                    for (int i = 0; i < array.Length; i++) array[i] = int.Parse(Console.ReadLine());
-                    break;
-                case 2:
-                    using (StreamReader sr = new StreamReader("input.txt"))
                     {
-                        string text = sr.ReadToEnd();
-                        string[] parts = text.Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-                        array = new int[parts.Length];
-                        for (int i = 0; i < parts.Length; i++) array[i] = int.Parse(parts[i]);
+                        Console.Write("N: ");
+                        array = new int[int.Parse(Console.ReadLine())];
+                        for (int i = 0; i < array.Length; i++) 
+                            array[i] = int.Parse(Console.ReadLine());
+                        break;
                     }
-                    break;
+                case 2:
+                    {
+                        using (StreamReader sr = new StreamReader("input.txt"))
+                        {
+                            string text = sr.ReadToEnd();
+                            string[] parts = text.Split(new[] { ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                            array = new int[parts.Length];
+                            for (int i = 0; i < parts.Length; i++) array[i] = int.Parse(parts[i]);
+                        }
+                        break;
+                    }
                 case 3:
-                    Console.Write("Розмір N: ");
-                    array = new int[int.Parse(Console.ReadLine())];
-                    for (int i = 0; i < array.Length; i++) array[i] = random.Next(-50, 51);
-                    break;
+                    {
+                        Console.Write("N: ");
+                        array = new int[int.Parse(Console.ReadLine())];
+                        for (int i = 0; i < array.Length; i++) array[i] = random.Next(-50, 51);
+                        break;
+                    }
             }
 
             if (array.Length < 2) return;
@@ -44,17 +51,29 @@ namespace LabApp3
 
             for (int i = 2; i < array.Length; i++)
             {
-                if (array[i] < array[min1]) { min2 = min1; min1 = i; }
-                else if (array[i] < array[min2]) { min2 = i; }
+                if (array[i] < array[min1]) 
+                { 
+                    min2 = min1; 
+                    min1 = i;
+                    continue;
+                }
+                if (array[i] < array[min2]) { 
+                    min2 = i; 
+                }
             }
 
-            int start = Math.Min(min1, min2), end = Math.Max(min1, min2);
-            int sum = 0, count = 0;
+            int start = min1, end = min2;
+            if (start > end)
+            {
+                start = min2;
+                end = min1;
+            }
+
+            int sum = 0, count = end - start - 1;
 
             for (int i = start + 1; i < end; i++)
             {
                 sum += array[i];
-                count++;
             }
 
             Console.WriteLine("\nСписок: " + string.Join(" ", array));
@@ -62,10 +81,9 @@ namespace LabApp3
 
             using (StreamWriter sw = new StreamWriter("output.txt"))
             {
-                sw.WriteLine("Результати розрахунку:");
+                sw.WriteLine("Результати:");
                 sw.WriteLine($"Мінімуми: {array[min1]}, {array[min2]}\nКількість між ними: {count}\nСума: {sum}");
             }
-            Console.WriteLine("\nРезультат також збережено в output.txt");
             Console.ReadKey();
         }
         public static void Task2()
@@ -85,72 +103,81 @@ namespace LabApp3
                 for (int j = 0; j < n; j++)
                     vectorZ[i] += matrixA[i, j] * vectorX[j];
 
-            Print("Вектор X", vectorX);
-            Print("Вектор Z", vectorZ);
+            Console.WriteLine("Вектор X", vectorX);
+            Console.WriteLine("Вектор Z", vectorZ);
 
             CheckAndSaveResult(vectorZ);
         }
 
         static double[,] FillMatrix(int m, int n)
         {
-            Console.WriteLine("\nОберіть спосіб: 1-Ручний, 2-Рандом, 3-Файл");
+            Console.WriteLine("\n1-Ручний, 2-Рандом, 3-Файл");
             int mode = int.Parse(Console.ReadLine());
             double[,] res = new double[m, n];
 
             switch (mode)
             {
                 case 1:
-                    for (int i = 0; i < m; i++)
-                        for (int j = 0; j < n; j++)
-                        {
-                            Console.Write($"A[{i},{j}] = ");
-                            res[i, j] = double.Parse(Console.ReadLine());
-                        }
-                    break;
-                case 2:
-                    for (int i = 0; i < m; i++)
-                        for (int j = 0; j < n; j++)
-                            res[i, j] = Math.Round(random.NextDouble() * 10, 2);
-                    break;
-                case 3:
-                    using (StreamReader sr = new StreamReader("input.txt"))
                     {
                         for (int i = 0; i < m; i++)
-                        {
-                            string line = sr.ReadLine();
-                            string[] values = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
                             for (int j = 0; j < n; j++)
-                                res[i, j] = double.Parse(values[j]);
-                        }
+                            {
+                                Console.Write($"A[{i},{j}] = ");
+                                res[i, j] = double.Parse(Console.ReadLine());
+                            }
+                        break;
                     }
-                    break;
+                case 2:
+                    {
+                        for (int i = 0; i < m; i++)
+                            for (int j = 0; j < n; j++)
+                                res[i, j] = Math.Round(random.NextDouble() * 10, 2);
+                        break;
+                    }
+                case 3:
+                    {
+                        using (StreamReader sr = new StreamReader("input.txt"))
+                        {
+                            for (int i = 0; i < m; i++)
+                            {
+                                string line = sr.ReadLine();
+                                string[] values = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                                for (int j = 0; j < n; j++)
+                                    res[i, j] = double.Parse(values[j]);
+                            }
+                        }
+                        break;
+                    }
             }
             return res;
         }
 
-        static void CheckAndSaveResult(double[] z)
+        static void CheckAndSaveResult(double[] val)
         {
-            bool ok = true;
-            int breakIdx = -1;
+            string resultMessage;
+            bool isSorted = true;
+            int problemIndex = -1;
 
-            for (int i = 0; i < z.Length - 1; i++)
-                if (z[i] > z[i + 1]) { ok = false; breakIdx = i + 1; break; }
-
-
-            using (StreamWriter sw = new StreamWriter("result.txt"))
+            for (int i = 0; i < val.Length - 1; i++)
             {
-                string msg = ok ? "Впорядковано." : $"Порушено на елементі {z[breakIdx]:F2} (індекс {breakIdx})";
-                Console.WriteLine($"\nРезультат: {msg}");
-                sw.WriteLine($"Результат перевірки: {msg}");
+                if (val[i] > val[i + 1])
+                {
+                    isSorted = false;
+                    problemIndex = i + 1;
+                    break;
+                }
             }
-            Console.WriteLine("Результат також збережено у file: result.txt");
-        }
 
-        static void Print(string title, double[] arr)
-        {
-            Console.Write($"\n{title}: ");
-            foreach (double x in arr) Console.Write($"{x:F2}  ");
-            Console.WriteLine();
+            using (StreamWriter writer = new StreamWriter("result.txt"))
+            {
+                if (isSorted)
+                    resultMessage = "Впорядковано.";
+                else
+                    resultMessage = $"Порушено на елементі {val[problemIndex]:F2} (індекс {problemIndex})";
+
+                Console.WriteLine("\nРезультат: " + resultMessage);
+                Console.WriteLine("Результат перевірки: " + resultMessage);
+            }
         }
 
         public static void Task3()
@@ -193,93 +220,106 @@ namespace LabApp3
         }
         static void AddRandomTile(int[,] board)
         {
-            List<int[]> emptyCells = new List<int[]>();
-            for (int i = 0; i < board.GetLength(0); i++)
-                for (int j = 0; j < board.GetLength(1); j++)
-                    if (board[i, j] == 0) emptyCells.Add(new[] { i, j });
+            int zeros = 0;
+            foreach (int v in board) if (v == 0) zeros++;
+            if (zeros == 0) return;
 
-            if (emptyCells.Count > 0)
+            int i, j;
+            do
             {
-                int[] cell = emptyCells[random.Next(emptyCells.Count)];
-                board[cell[0], cell[1]] = random.Next(100) < 90 ? 2 : 4;
-            }
-        }
+                i = random.Next(board.GetLength(0));
+                j = random.Next(board.GetLength(1));
+            } while (board[i, j] != 0);
 
-        static int ProcessLine(int[] line)
-        {
-            int score = 0;
-            int size = line.Length;
-
-            int[] nextLine = new int[size];
-            int pos = 0;
-            for (int i = 0; i < size; i++)
-                if (line[i] != 0) nextLine[pos++] = line[i];
-            for (int i = 0; i < size - 1; i++)
-            {
-                if (nextLine[i] != 0 && nextLine[i] == nextLine[i + 1])
-                {
-                    nextLine[i] *= 2;
-                    score += nextLine[i];
-                    nextLine[i + 1] = 0;
-                }
-            }
-            for (int i = 0; i < size; i++) line[i] = 0;
-            pos = 0;
-            for (int i = 0; i < size; i++)
-                if (nextLine[i] != 0) line[pos++] = nextLine[i];
-
-            return score;
+            board[i, j] = random.Next(100) < 90 ? 2 : 4;
         }
 
         static int MoveLeft(int[,] board)
         {
-            int totalScore = 0;
+            int score = 0;
             for (int i = 0; i < board.GetLength(0); i++)
-            {
-                int[] row = new int[board.GetLength(1)];
-                for (int j = 0; j < board.GetLength(1); j++) row[j] = board[i, j];
-                totalScore += ProcessLine(row);
-                for (int j = 0; j < board.GetLength(1); j++) board[i, j] = row[j];
-            }
-            return totalScore;
-        }
-        static int MoveRight(int[,] board)
-        {
-            int totalScore = 0;
-            for (int i = 0; i < board.GetLength(0); i++)
-            {
-                int[] row = new int[board.GetLength(1)];
-                for (int j = 0; j < board.GetLength(1); j++) row[j] = board[i, board.GetLength(1) - 1 - j];
-                totalScore += ProcessLine(row);
-                for (int j = 0; j < board.GetLength(1); j++) board[i, board.GetLength(1) - 1 - j] = row[j];
-            }
-            return totalScore;
-        }
-        static int MoveUp(int[,] board)
-        {
-            int totalScore = 0;
-            for (int j = 0; j < board.GetLength(1); j++)
-            {
-                int[] col = new int[board.GetLength(0)];
-                for (int i = 0; i < board.GetLength(0); i++) col[i] = board[i, j];
-                totalScore += ProcessLine(col);
-                for (int i = 0; i < board.GetLength(0); i++) board[i, j] = col[i];
-            }
-            return totalScore;
-        }
-        static int MoveDown(int[,] board)
-        {
-            int totalScore = 0;
-            for (int j = 0; j < board.GetLength(1); j++)
-            {
-                int[] col = new int[board.GetLength(0)];
-                for (int i = 0; i < board.GetLength(0); i++) col[i] = board[board.GetLength(0) - 1 - i, j];
-                totalScore += ProcessLine(col);
-                for (int i = 0; i < board.GetLength(0); i++) board[board.GetLength(0) - 1 - i, j] = col[i];
-            }
-            return totalScore;
+                for (int j = 1; j < board.GetLength(1); j++)
+                    if (board[i, j] != 0)
+                        for (int k = j; k > 0; k--)
+                        {
+                            if (board[i, k - 1] == 0) {
+                                board[i, k - 1] = board[i, k];
+                                board[i, k] = 0;
+                                continue;
+                            }
+                            if (board[i, k - 1] == board[i, k]) {
+                                board[i, k] = 0;
+                                score += (board[i, k - 1] *= 2);
+                            }
+                            break;
+                        }
+            return score;
         }
 
+        static int MoveRight(int[,] board)
+        {
+            int score = 0;
+            for (int i = 0; i < board.GetLength(0); i++)
+                for (int j = board.GetLength(1) - 2; j >= 0; j--)
+                    if (board[i, j] != 0)
+                        for (int k = j; k < board.GetLength(1) - 1; k++)
+                        {
+                            if (board[i, k + 1] == 0) {
+                                board[i, k + 1] = board[i, k];
+                                board[i, k] = 0;
+                                continue;
+                            }
+                            if (board[i, k + 1] == board[i, k]) {
+                                board[i, k] = 0;
+                                score += (board[i, k + 1] *= 2);
+                            }
+                            break;
+                        }
+            return score;
+        }
+
+        static int MoveUp(int[,] board)
+        {
+            int score = 0;
+            for (int j = 0; j < board.GetLength(1); j++)
+                for (int i = 1; i < board.GetLength(0); i++)
+                    if (board[i, j] != 0)
+                        for (int k = i; k > 0; k--)
+                        {
+                            if (board[k - 1, j] == 0) {
+                                board[k - 1, j] = board[k, j];
+                                board[k, j] = 0;
+                                continue;
+                            }
+                            if (board[k - 1, j] == board[k, j])
+                            {
+                                board[k, j] = 0;
+                                score += (board[k - 1, j] *= 2);
+                            }
+                            break;
+                        }
+            return score;
+        }
+
+        static int MoveDown(int[,] board)
+        {
+            int score = 0;
+            for (int j = 0; j < board.GetLength(1); j++)
+                for (int i = board.GetLength(0) - 2; i >= 0; i--)
+                    if (board[i, j] != 0)
+                        for (int k = i; k < board.GetLength(0) - 1; k++)
+                        {
+                            if (board[k + 1, j] == 0) {
+                                board[k + 1, j] = board[k, j];
+                                board[k, j] = 0; continue;
+                            }
+                            if (board[k + 1, j] == board[k, j]) {
+                                board[k, j] = 0; score += (board[k + 1, j] *= 2);
+                            }
+                            break;
+                        }
+            return score;
+        }
         static void SaveToFile(int[,] board)
         {
             using (StreamWriter sw = new StreamWriter("save.txt"))
@@ -291,7 +331,6 @@ namespace LabApp3
                     sw.WriteLine();
                 }
             }
-            Console.WriteLine("Збережено!");
         }
 
         static int[,] LoadFromFile(int size)
